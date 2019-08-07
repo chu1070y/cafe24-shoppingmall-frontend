ff0d90e3ffd9e282acd28c141f6fb26da9ce5e4f
chu1070y <chu1070y@naver.com>
2019-08-07 21:57:16 +0900

    쇼핑몰 메인화면 개발 중

--- shoppingmall/views.py
+++ shoppingmall/views.py
@@ -1,6 +1,16 @@
+import json
+
 from django.shortcuts import render
 
-
 # Create your views here.
+from shoppingmall_frontend.settings import MEDIA_ROOT
+from shoppingmall_frontend.utils import RestAPI
+
+rest_api = RestAPI()
+
+
 def index(request):
-    return render(request, 'shoppingmall/index.html')
+    product_list = json.loads(rest_api.api_get("/api/product/list"))
+    print(MEDIA_ROOT)
+
+    return render(request, 'shoppingmall/index.html', {'product_list': product_list})
